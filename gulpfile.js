d8ff41b2dd072a7085dae6fb05a5e5fc82f30f0b
Sebastian Webber <sebastian@swebber.me>
2016-11-05 16:04:14 -0200

    adding gulp deps to compile the sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
+'use strict';
+
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var del = require('del');
 var exec = require('child_process').execSync;
 var path = require('path');
 var gutil = require('gulp-util');
+var sass = require('gulp-sass');
 
 
 // Based on: https://github.com/jbrodriguez/hugulp/blob/master/gulp/hugo.js
@@ -31,6 +34,17 @@ gulp.task('clean:dist', function () {
     return del([
         './dist/**/*'
     ]);
+});
+
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/static/css/'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 
