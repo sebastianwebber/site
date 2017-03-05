65260833d6bbc3cdf952f6b8cdb7f02fb11af7fc
Sebastian Webber <sebastian@swebber.me>
2017-03-05 00:05:39 -0300

    customizing de sass file and adding to the html file

--- gulpfile.js
+++ gulpfile.js
@@ -38,13 +38,23 @@ gulp.task('clean:dist', function () {
 
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src('./sass/**/*.s*ss')
+    .pipe(
+      sass(
+        {
+          includePaths: [
+            './node_modules/bulma',
+          ],
+          errLogToConsole: true
+        }
+      )
+      .on('error', sass.logError)
+    )
     .pipe(gulp.dest('./src/static/css/'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.s*ss', ['sass']);
 });
 
 
