715c84a3feaa56ecb3110b0ec568bb890a79cba9
Sebastian Webber <sebastian@swebber.me>
2017-05-12 13:32:13 -0300

    Update 2017-05-12T16:32:10.622Z

--- js/bulma.js
+++ js/bulma.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function ($) {
 
   var $toggle = $('#nav-toggle');
-  var $menu = $('#nav-menu');
+  var $menuLeft = $('#nav-menu-left');
+  var $menuRight = $('#nav-menu-right');
 
   $toggle.click(function() {
     $(this).toggleClass('is-active');
-    $menu.toggleClass('is-active');
+    $menuLeft.toggleClass('is-active');
+    $menuRight.toggleClass('is-active');
   });
 });
